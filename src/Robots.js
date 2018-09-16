cfec3262246b5c8362aa472babbbb761c554bac8
Peters Gideon Ojoajogu <gideonpeters85@gmail.com>
2018-09-16 15:36:41 +0100

    Add files via upload

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const Robots = [
+{
+	id: 1,
+	name: 'Ebube Donald',
+	username: 'manlikeked',
+	email: 'donaldebube@gmail.com'
+},
+{
+	id: 2,
+	name: 'Igebu Anesi',
+	username: 'theanesiigebu',
+	email: 'gabriellaigebu@gmail.com'
+},
+{
+	id: 3,
+	name: 'Kanu Kelechi',
+	username: 'kaytez_kanu',
+	email: 'kaik@yahoo.com'
+},
+{
+	id: 4,
+	name: 'Odejayi Tosin',
+	username: 'folarin_t',
+	email: 'tosinodejayi@gmail.com'
+},
+{
+	id: 5,
+	name: 'Etuk Jeremiah',
+	username: 'etukjerry',
+	email: 'jeremiah.etuk@stu.cu.edu.ng'
+},
+{
+	id: 6,
+	name: 'William Imoukhuede',
+	username: 'willingtoeatyam',
+	email: 'williampromise@gmail.com'
+},
+{
+	id: 7,
+	name: 'David Dave Michael',
+	username: 'idavidmichael',
+	email: 'princedavitt@gmail.com'
+},
+{
+	id: 8,
+	name: 'Abu Jeremiah',
+	username: 'jeri.miah',
+	email: 'jerimiah093@gmail.com'
+},
+{
+	id: 9,
+	name: 'Onoja Favour',
+	username: 'eleojo_',
+	email: 'favouronoja@gmail.com'
+},
+{
+	id: 10,
+	name: 'Shobande Josephine',
+	username: 'josepheenah',
+	email: 'josepheenah@gmail.com'
+}
+]
